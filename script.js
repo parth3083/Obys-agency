4d3af1a5d8c933221204a22334c4c1cfeefa6258
parth3083 <parthbrajput30@gmail.com>
2024-02-21 07:31:24 +0530

    Added page 2

--- script.js
+++ script.js
@@ -54,7 +54,35 @@ function magnetEffect() {
     Shery.makeMagnet("#right h4");
 Shery.makeMagnet("nav #left i");
 }
+function page2Effect() { 
+    var p2_limit = document.querySelector("#video-container");
+var image = document.querySelector("#image");
+var vid = document.querySelector("#vid");
+var crsr=document.querySelector("#video-crsr");
+p2_limit.addEventListener("mousemove", function (dets) {
+    var y = dets.y;
+    var x = dets.x;
+    var r = y - 100;
+    var t = x - 545;
+    gsap.to(crsr, {
+        top: r,
+        left: t
+    })
+});
+crsr.addEventListener("click", function () {
+    image.style.opacity = 0;
+});
+crsr.addEventListener("click", function () {
+    crsr.style.opacity = 0;
+});
+p2_limit.addEventListener("mouseleave", function () {
+    image.style.opacity = 1;
+    crsr.style.opacity = 1;
+});
+}
+
 
 cursurAnimation();
 loaderAnimation();
 magnetEffect();
+page2Effect();
